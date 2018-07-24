913cad52d4b62d5d13dbd2fa31e8a41ac18492dd
jainbhavya65 <jainbhavya65@gmail.com>
2018-07-24 20:01:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
+image_name=jainbhavya65/jenkins
+i=$(sudo docker images | grep -i jainbhavya65/error | cut -d ' '  -f26 | wc -l )
+i=$(expr $i + 1)
 if [ "$JOB_NAME"=="Git-copy" ];
 then
 git clone https://github.com/jainbhavya65/nodejs.git
 fi
+if [ "$JOB_NAME"=="docker-build" ];
+then
+cd nodejs
+docker run -t $image_name:$i .
+if
