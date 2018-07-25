4c7d330db84add4d55b6b56a11086928b70bacd4
jainbhavya65 <jainbhavya65@gmail.com>
2018-07-25 12:11:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+
 image_name=jainbhavya65/jenkins
 i=$(sudo docker images | grep -i jainbhavya65/jenkins | cut -d ' '  -f26 | wc -l )
 if [ "$JOB_NAME" = "Git-copy" ];
@@ -14,6 +15,6 @@ then
 sudo docker run $image_name:$i /bin/cat server.js
 elif [ "$JOB_NAME" = "docker-push" ];
 then
-sudo docker login -u jainbhavya65 -p rX8n6zUW8BMf8j39yl4HJl1
+sudo docker login --username=$dockeruser --password=$dockerpassword
 sudo docker push $image_name:$i 
 fi 
