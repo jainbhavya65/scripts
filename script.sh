a4afaf9806d283c3f6c8338227bff4d20eb6466a
jainbhavya65 <jainbhavya65@gmail.com>
2018-07-25 10:47:18 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 image_name=jainbhavya65/jenkins
 i=$(sudo docker images | grep -i jainbhavya65/jenkins | cut -d ' '  -f26 | wc -l )
-i=$(expr $i + 1)
 if [ "$JOB_NAME" = "Git-copy" ];
 then
 git clone https://github.com/jainbhavya65/nodejs.git
 sudo rsync -Parv nodejs ../../
 elif [ "$JOB_NAME" = "docker-build" ];
 then
+i=$(expr $i + 1)
 cd ../../nodejs
 sudo docker build -t $image_name:$i .
 elif [ "$JOB_NAME" = "docker-testing" ];
