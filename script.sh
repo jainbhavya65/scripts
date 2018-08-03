bfd82a3b9f2895842a29043f619d2cee086e44f2
jainbhavya65 <jainbhavya65@gmail.com>
2018-08-03 12:24:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$JOB_NAME" = "Git-copy" ];
 then
-mkdir ../../project1
-mv scripts/project1-script.sh ../../project1/
-cd ../../project1
+mkdir ../project1
+mv scripts/project1-script.sh ../project1/
+cd ../project1
 bash project1-script.sh
 rm -rf *
 fi
