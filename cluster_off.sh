60f3f2d54060aa41717f10563841588423f0bdf2
iface14 <bhavya@iface.io>
2018-08-22 13:21:03 +0530

    cluster_off_update

--- cluster_off.sh
+++ cluster_off.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 rm -f test.txt
 rm -f cluster_no.txt
-read -p "Enter Project Name:" projectname
+rm -f project_list.txt
+gcloud projects list | awk '{print $1}' | tail -n +2 >> project_list.txt
+awk '{print NR,$0}' project_list.txt | tee project_select.txt
+read -p "Select Project:" P_Select
+projectname=$(sed -sn "$P_Select"p project_select.txt | cut -d ' ' -f2 )
 #read -p "Enter No of Node(if you want to temporary down it just enter it 0):" resize
+echo "Selected Project:" $projectname
 y=$(echo "set")
 gcloud config $y project $projectname
 cluster_list=$(gcloud container clusters list | awk '{print $1}' | tail -n +2)
@@ -18,13 +23,13 @@ read -p "Enter Option(for down all Just Enter all):" o
          echo "Selected Cluster Name:" $cluster
          pool_list=$(gcloud container  node-pools list --cluster $cluster| awk '{print $1}' | tail -n +2)
          pool_name=$(gcloud container  node-pools describe $pool_list --cluster $cluster | grep compute/v1 | awk -F '/' '{print $11}'|awk -F '-' '{print $2"-"$3"-"$4}')
-         gcloud container clusters update $cluster  --no-enable-autoscaling
-         gcloud container node-pools update $(gcloud container node-pools list --cluster $cluster | awk {'print $1'} | tail -n +2) --cluster $cluster --no-enable-autorepair
+         #gcloud container clusters update $cluster  --no-enable-autoscaling
+         #gcloud container node-pools update $(gcloud container node-pools list --cluster $cluster | awk {'print $1'} | tail -n +2) --cluster $cluster --no-enable-autorepair
          nodes_name=$(gcloud compute instances list | awk '{print $1}'| grep -i $pool_name)
           for z in $nodes_name
           do
            echo $z             
-           gcloud compute instances stop $z
+           #gcloud compute instances stop $z
           done
    else
 	echo "Selected Cluster Name: ALL cluster are going to down" 
@@ -34,6 +39,6 @@ read -p "Enter Option(for down all Just Enter all):" o
           for z in $node_all
           do
 	   echo $z
-           gcloud compute instances stop $z
+           #gcloud compute instances stop $z
           done
 fi
