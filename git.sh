d6e97f8caa2fdb5aa2296099ffec478494d48ccd
iface14 <bhavya@iface.io>
2018-08-23 19:57:44 +0530

    test

--- git.sh
+++ git.sh
@@ -2,9 +2,8 @@ git init
 git add .
 git commit -m "test"
 user=$(zenity --password --username 2> /dev/null)
-IFS="|" read -r username password   <<< "$user"
+IFS="|" read -r username password   <<< "$user";
 git push << EOF
-echo $username
 $username
-echo $password
+$password
 EOF
