bf9d18063cd33a32412c92ee63a5145bfd3f64c8
jainbhavya65@gmail.com <bhavya@iface.io>
2018-08-28 11:42:31 +0530

    commit

--- config.development.ts
+++ config.development.ts
@@ -1,6 +1,6 @@
 export const ApiConfig = {
 apiUrl: 'http://localhost:7000/',
-//apiUrl: 'https://crm-api.iface.io/',
+////apiUrl: 'https://crm-api.iface.io/',
 apiPath : 'api/v1/',
 
 seachurl:'https://api.datamuse.com'
